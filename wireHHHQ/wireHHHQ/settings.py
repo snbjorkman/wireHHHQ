b98e8333c8bae73e5005704949284906c52135cf
Samuel Bjorkman <samuelbjorkman@Samuels-MacBook-Air.local>
2025-05-29 12:37:01 -0600

    linked db, set up app and views

--- wireHHHQ/wireHHHQ/settings.py
+++ wireHHHQ/wireHHHQ/settings.py
@@ -72,11 +72,25 @@ WSGI_APPLICATION = 'wireHHHQ.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'wirehhhq',
+
+        'USER': 'wiredev',
+
+        'PASSWORD': 'twotrue',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
+
 }
 
 
