4bca85acd81b1ab7c62179a1a9c049961fa0e3ef
Samuel Bjorkman <samuelbjorkman@Samuels-MacBook-Air.local>
2025-05-29 13:17:54 -0600

    created models; api interface for project is yet to be completed. testing will have to be done for the models as they are right now.

--- wireHHHQ/core/models.py
+++ wireHHHQ/core/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+class Color(models.Model):
+    color_id = models.CharField(max_length=30, primary_key=True)
+    color_desc = models.CharField(max_length=30)
+
+
+class Brand(models.Model):
+    brand_id = models.CharField(max_length=30, primary_key=True)
+    brand_desc = models.CharField(max_length=30)
+
+
+class WireType(models.Model):
+    wire_type_id = models.CharField(max_length=30, primary_key=True)
+    wire_type_desc = models.CharField(max_length=30)
+
+
+class Location(models.Model):
+    location_id = models.CharField(max_length=30, primary_key=True)
+    location_desc = models.CharField(max_length=30)
+
+
+class Project(models.Model):
+    project_id = models.CharField(max_length=30, primary_key=True)
+
+
+class WireSku(models.Model):
+    sku = models.CharField(max_length=50, primary_key=True)
+    wire_type_id = models.ForeignKey(WireType)
+    brand_id = models.ForeignKey(Brand)
+    color_id = models.ForeignKey(Color)
+    feet = models.IntegerField()
+    typical_cost = models.FloatField()
+    cost_per_foot = models.FloatField() #this will be computed in the view, maybe in a js function, not in the model.
+    
+
+class WireBox(models.Model):
+    wire_box_id = models.CharField(max_length=30, primary_key=True)
+    sku = models.ForeignKey(WireSku)
+    location_id = models.ForeignKey(Location)
+    purchase_cost = models.IntegerField()
+    purchase_date = models.DateField()
+    disposed_date = models.DateField(null=True)
+    disposed_feet_rem = models.DateField(null=True)
+
+
+class WireUsage(models.Model):
+    wire_usage_id = models.CharField(max_length=30, primary_key=True)
+    wire_box_id = models.ForeignKey(WireBox)
+    start_feet = models.IntegerField()
+    end_feet = models.IntegerField()
+    date_used = models.DateField()
+    project_id = models.ForeignKey(Project)
